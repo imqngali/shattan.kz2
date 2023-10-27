5b75d304926c8a80ad117ae5b3ad3fccd8853687
imqngali <55797666+imqngali@users.noreply.github.com>
2023-10-27 23:11:22 +0600

    form

---
+++ telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot6310315793:AAEs9fozXnve3Eq0Ffc3-EGUwmz1olbo1oc/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "bot6310315793:AAEs9fozXnve3Eq0Ffc3-EGUwmz1olbo1oc";
+$chat_id = "-4094313875";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
